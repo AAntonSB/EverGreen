c8c878158d710d0288dc0a1d58fb3766f57cd792
AAntonSB <anton.s.bergqvist@gmail.com>
2020-05-02 18:59:38 +0200

    Jag tror jag har lagt upp de nu!

---
+++ Assets/CloudManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class CloudManager : MonoBehaviour
+{
+
+    public int cloudPartAmount = 5;
+    GameObject cloudpart;
+    public float randomVectorRange = 200;
+    Vector3 randomVector3;
+    public float speed = 1;
+
+
+
+    private void Update()
+    {
+        
+        transform.position=transform.position+transform.forward*speed;
+
+
+    }
+
+    void Start()
+    {
+
+        
+
+        GenerateCloudParts();
+
+        GenerateCloud();
+
+        transform.gameObject.AddComponent<MeshCombiner>();
+        transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+
+    }
+
+    void GenerateRandomVector3()
+    {
+
+        randomVector3 = new Vector3(
+            Random.Range(-randomVectorRange, randomVectorRange),
+            0,
+            Random.Range(-randomVectorRange, randomVectorRange));
+
+    }
+
+    void GenerateCloud()
+    {
+
+        for (int i = 0; i < cloudPartAmount; i++)
+        {
+
+            GenerateRandomVector3();
+            GenerateCloudParts();
+
+            (Instantiate(cloudpart, transform.position+randomVector3, new Quaternion(Random.Range(0,10), Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10))) as GameObject).transform.parent = transform;
+
+            
+
+        }
+    }
+
+
+    void GenerateCloudParts()
+    {
+        cloudpart = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        cloudpart.GetComponent<MeshRenderer>().material.color = Color.white;
+
+        Vector3 cloudPartSize = new Vector3(Random.Range(20, 50), Random.Range(20, 50), Random.Range(20, 50));
+        cloudpart.transform.localScale = cloudPartSize;
+
+        Destroy(cloudpart);
+        
+
+    }
+    
+
+
+    
+
+
+   
+}
---
+++ Assets/Scripts/CameraSpectate.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class CameraSpectate : MonoBehaviour
+{
+
+	public GameObject tree;
+
+	Vector2 rotation = new Vector2(0, 0);
+	public float speed = 3;
+	public float flyspeed;
+	void Update()
+	{
+		rotation.y += Input.GetAxis("Mouse X");
+		rotation.x += -Input.GetAxis("Mouse Y");
+		transform.eulerAngles = (Vector2)rotation * speed;
+
+		float V = Input.GetAxis("Vertical");
+		float H = Input.GetAxis("Horizontal");
+
+		flyspeed = 20;
+
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			flyspeed = flyspeed * 2;
+
+		}
+
+		transform.position = transform.position + (V*transform.forward *flyspeed* Time.deltaTime);
+		transform.position = transform.position + (H * transform.right * flyspeed * Time.deltaTime);
+
+
+
+
+		RaycastHit hit;
+
+		Physics.Raycast(transform.position, transform.forward, out hit, 1000);
+
+		Debug.Log(hit.transform.position);
+
+		//bool hasspawned = false;
+
+			if(Input.GetMouseButtonDown(0))
+		{
+			Instantiate(tree, transform.position+transform.forward*hit.distance, tree.transform.rotation);
+			//hasspawned = true;
+		
+		}
+		
+			//if(Input.GetMouseButtonDown
+
+
+	}
+}
