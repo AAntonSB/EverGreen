87c86d32d1ff9bd7c99667a2957d9ed18a5a4257
AAntonSB <anton.s.bergqvist@gmail.com>
2020-05-07 15:57:24 +0200

    World Generation & VictoryState

---
+++ Assets/Scripts/Managers/StateManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public GameObject player;
+    public Canvas winScreen;
+    public float VictoryLength = 100;
+
+
+    private void Update()
+    {
+        if(player.transform.position.x >= VictoryLength || player.transform.position.z >= VictoryLength ||
+           player.transform.position.x <= -VictoryLength || player.transform.position.z <= -VictoryLength)
+        {
+            winScreen.gameObject.SetActive(true);
+        }
+    }
+}
